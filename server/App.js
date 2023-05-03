c075d3ad912c746db1d9fa2241bcb0c998ae580d
yousefelsemeen <yousefelsemeen@gmail.com>
2023-05-03 16:32:23 +0300

    Add basic folder structure of the project and setup react, mongoose, dotenv and nodemon.

---
+++ server/App.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
