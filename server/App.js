f9554843c137fccd4182cac25396ac2fc9808215
yousefelsemeen <yousefelsemeen@gmail.com>
2023-05-06 20:03:34 +0300

    delete some default react files

--- server/App.js
+++ server/App.js
@@ -8,7 +8,7 @@ const app = express();
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(err));
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
