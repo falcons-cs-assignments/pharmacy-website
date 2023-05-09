ba46713d9cdaf2cabe7b6f127ac80738e28fefd7
yousefelsemeen <yousefelsemeen@gmail.com>
2023-05-09 21:12:13 +0300

    add database models

--- server/App.js
+++ server/App.js
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.error(err));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
---
+++ server/src/models/Product.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+});
+
+export const Product = mongoose.model("products", productSchema);
