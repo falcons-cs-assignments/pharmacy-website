e48cef4d478149cd84a0bd2eb13196314cd9b47b
ahmad-shaban <ahmedshaban0124@gmail.com>
2023-05-22 20:27:21 +0300

    use dotenv to define API_URL for client

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 	app.use(
 		"/api",
 		createProxyMiddleware({
-			target: "https://pharmacy-website-api.onrender.com",
+			target: process.env.API_URL,
 			changeOrigin: true,
 		})
 	);
